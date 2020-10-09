e1e56cf19b6f727ae9bfe497e0c771a186e8f37d
ENDER-9 <julian.jaksch@gmx.de>
2020-10-09 22:40:28 +0200

    added NewVocabHandler class

---
+++ src/NewVocabHandler.cpp
@@ -0,0 +1,5 @@
+#include "NewVocabHandler.hpp"
+
+NewVocabHandler::NewVocabHandler(const Database &db) : database(db){
+
+}
---
+++ src/NewVocabHandler.hpp
@@ -0,0 +1,18 @@
+#ifndef NEWVOCABHANDLER_H
+#define NEWVOCABHANDLER_H
+
+#include "database.hpp"
+#include <vector>
+
+class NewVocabHandler {
+
+public:
+    NewVocabHandler(const Database &db);
+
+
+private:
+    Database database;
+
+};
+
+#endif //NEWVOCABHANDLER_H
