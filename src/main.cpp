a5bcbb1a56f73ea709c15a957f8b399bd56d7a35
ENDER-9 <julian.jaksch@gmx.de>
2020-10-09 22:40:01 +0200

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QIcon>
+#include "database.hpp"
+#include "FrontendHandler.hpp"
 
 int main(int argc, char *argv[])
 {
+    //create database
+    Database database("database");
+    //database.checkTableEmpty("language_package");
+
+    qmlRegisterType<FrontendHandler>("com.shsSolutions.FrontendHandler", 1, 0, "FrontendHandler");
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    
 
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/src/qml/main.qml"));
-    app.setWindowIcon(QIcon(":/src/images/logo.png"));
+    app.setWindowIcon(QIcon(":/src/images/logo-56px.png"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
