add4cd5a5b8c57b23d9fc8c5884006e6839e1bdc
noctera <julian.jaksch@gmx.de>
2020-10-22 17:05:54 +0200

    added file for language pack

---
+++ frontend/src/Components/Others/Dropdown/Examples.js
@@ -0,0 +1,27 @@
+let location = [
+  {id: 0, title: 'New York', key: 'location'},
+  {id: 1, title: 'Dublin', key: 'location'},
+  {id: 2, title: 'Istanbul', key: 'location'},
+  {id: 3, title: 'New York', key: 'location'},
+  {id: 4, title: 'Dublin', key: 'location'},
+  {id: 5, title: 'Istanbul', key: 'location'},
+  {id: 6, title: 'New York', key: 'location'},
+  {id: 7, title: 'Dublin', key: 'location'},
+  {id: 8, title: 'New York', key: 'location'},
+  {id: 9, title: 'Dublin', key: 'location'},
+  {id: 10, title: 'Istanbul', key: 'location'},
+  {id: 11, title: 'New York', key: 'location'},
+  {id: 12, title: 'Dublin', key: 'location'},
+  {id: 13, title: 'Istanbul', key: 'location'},
+  {id: 14, title: 'New York', key: 'location'},
+  {id: 15, title: 'Dublin', key: 'location'},
+  {id: 16, title: 'Istanbul', key: 'location'},
+  {id: 17, title: 'New York', key: 'location'},
+  {id: 18, title: 'Dublin', key: 'location'},
+  {id: 19, title: 'Istanbul', key: 'location'},
+  {id: 20, title: 'New York', key: 'location'},
+  {id: 21, title: 'Dublin', key: 'location'},
+  {id: 22, title: 'Istanbul', key: 'location'}
+];
+
+export default location;
