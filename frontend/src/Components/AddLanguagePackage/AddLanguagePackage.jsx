a9e80d7a14410fe116d02ea1439ca14b52036469
noctera <julian.jaksch@gmx.de>
2020-10-28 16:11:31 +0100

    implementation of AddPackageLanguage backend

--- frontend/src/Components/AddLanguagePackage/AddLanguagePackage.jsx
+++ frontend/src/Components/AddLanguagePackage/AddLanguagePackage.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import InputField from '../Others/InputField/InputField';
-import DropdownMenu from '../Others/Dropdown/Dropdown';
+import Dropdown from '../Others/Dropdown/Dropdown';
+import languages from './Languages';
 
 function AddLanguagePackage(props) {
     const vocascanModule = require('../../../build/Debug/vocascan.node');
 
+    const [name, setName] = useState('');
+    const [foreignLanguage, setForeignLanguage] = useState('');
+    const [translatedLanguage, setTranslatedLanguage] = useState('');
+    const [vocabsPerDay, setVocabsPerDay] = useState(100);
+    const [rightTranslations, setRightTranslations] = useState(2);
+
     function submitHandler() {
-        vocascanModule.addLanguagePackage("Englisch - Deutsch", "Englisch", "Deutsch", 100, 2);
-        console.log("Added");
+        vocascanModule.addLanguagePackage(name, foreignLanguage, translatedLanguage, vocabsPerDay, rightTranslations);
         props.function()
     }
 
@@ -15,28 +21,28 @@ function AddLanguagePackage(props) {
         <form className="lngpckg">
             <label>
                 <h3>Name</h3>
-                <InputField placeholder="z.B. Englisch - Deutsch" />
+                <InputField placeholder="z.B. Englisch - Deutsch" function={e => setName(e.target.value)}/>
             </label>
 
             <div className="lngpckg-dropdown">
                 <label className="lngpckg-dropdown-field">
                     Fremdsprache
-                    <DropdownMenu title="Auswählen..." />
+                    <Dropdown title="Auswählen..." function={e => setForeignLanguage(e.target.value)} selection={languages}/>
                 </label>
                 <label className="lngpckg-dropdown-field">
                     Übersetzung
-                    <DropdownMenu title="Auswählen..." />
+                    <Dropdown title="Auswählen..." function={e => setTranslatedLanguage(e.target.value)} selection={languages}/>
                 </label>
             </div>
 
             <label>
                 <h3>Vokabeln pro Tag</h3>
-                <InputField placeholder="z.B. 100" />
+                <InputField placeholder="z.B. 100" function={e => setVocabsPerDay(e.target.value)}/>
             </label>
 
             <label>
                 <h3>Richtige Übersetzungen um Vokabelpaar erfolgreich zu haben</h3>
-                <InputField placeholder="z.B. 2" />
+                <InputField placeholder="z.B. 2" function={e => setRightTranslations(e.target.value)}/>
             </label>
 
             <button className="lngpckg-submitBtn" type="button" value="Submit" onClick={submitHandler}>Weiter</button>
--- frontend/src/Components/Others/InputField/InputField.jsx
+++ frontend/src/Components/Others/InputField/InputField.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function InputField(props) {
     return (
         <div className="inputField">
-            <input name="inputField-input" placeholder={props.placeholder} type="text" />
+            <input name="inputField-input" placeholder={props.placeholder} type="text" onChange={props.function}/>
         </div>
     )
 }
