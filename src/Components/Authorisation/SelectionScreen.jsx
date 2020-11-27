e80bb81e9957ba8dffd9d4ca83e25126da5dee48
Incognitus-9 <julian.jaksch@gmx.de>
2020-11-27 18:00:51 +0100

    added selection boxes to login screen

--- src/Components/Authorisation/SelectionScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function SelectionScreen(props) {
-    return (
-        <h1>Hello World</h1>
-    );
-}
-
-export default SelectionScreen;
---
+++ src/Components/Authorisation/SelectionScreen/SelectionScreen.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import SelectionBox from './SelectionBox.jsx';
+import './SelectionScreen.scss';
+import Image from './vocascan-logo.png';
+
+function SelectionScreen(props) {
+    return (
+        <div className="selectScrn">
+            <h1 className="selectScrn-title">LOG IN</h1>
+            <h1 className="selectScrn-heading">SELECT YOUR OPTION</h1>
+            <div className="boxes">   
+                <SelectionBox heading="Vocascan Server" description="Use the Vocascan Server and get very much advantages" image={Image}/>
+                <SelectionBox heading="Own Server" description="Use your own server" image={Image}/>
+            </div>
+        </div>
+    );
+}
+
+export default SelectionScreen;
