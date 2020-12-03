d2ebe05bbf661379c338657371aefff9a8087304
Incognitus-9 <julian.jaksch@gmx.de>
2020-12-03 18:19:56 +0100

    added new login and register route

--- src/app.jsx
+++ src/app.jsx
@@ -1,39 +1,43 @@
 import React, { useState } from 'react';
-import { Route, HashRouter } from "react-router-dom";
+import { Route, HashRouter, Redirect } from "react-router-dom";
 import './Components/sections.scss';
 import { useSelector } from 'react-redux';
+import Image from './images/vocascan-logo.png';
 
 
 
 import Nav from './Components/Nav/Nav.jsx';
 import AddVocab from './Components/AddVocab/AddVocab.jsx';
 import SelectionScreen from './Components/Authorisation/SelectionScreen/SelectionScreen.jsx';
+import Login from './Components/Authorisation/Login/Login.jsx';
+import Register from './Components/Authorisation/Register/Register.jsx';
 
 
 function App() {
 
   const isLoggedIn = useSelector(state => state.login.isLoggedIn);
-  const [popupOpen, setPopupOpen] = useState(true);
-
-  function togglePopup() {
-    setPopupOpen(false)
-  }
 
   if (!isLoggedIn) {
     return (
-      <SelectionScreen />
+      <HashRouter>
+          <Route path="/">
+            <Redirect to="/plans"/>
+          </Route>
+        <Route path="/plans" component={SelectionScreen } />
+        <Route path="/login" component={() => <Login image={Image} />} />
+        <Route path="/register" component={Register} />
+      </HashRouter>
+      
     );
   }
   else {
     return (
-    <div>
       <HashRouter>
         <div className="root">
           <Nav />
           <Route path="/addVocab" component={AddVocab} />
         </div>
-      </HashRouter>
-    </div>);
+      </HashRouter>);
   }
   
 }
