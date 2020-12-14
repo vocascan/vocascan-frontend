89de74f661d29d8cd109f2ef3c42903f1196e0f7
noctera <julian.jaksch@gmx.de>
2020-12-14 11:12:39 +0100

    added server call function and redux login

--- src/Components/Authorisation/Login/Login.jsx
+++ src/Components/Authorisation/Login/Login.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.scss';
 import { useHistory } from "react-router-dom";
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import {useDispatch} from "react-redux"
+import { login } from "../../../redux/Actions/index.js";
 
 function Login(props) {
+
+    //variables
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState(""); 
+    const [errorMsg, setErrorMsg] = useState(false);
+
+    const { register, handleSubmit, getValues, watch, errors } = useForm();
+    const dispatch = useDispatch();
+
 
     let history = useHistory();
     function handleClickRegister() {
         history.push("/register");
+    }
+
+    //make api call to login
+    async function submitLogin() {
+        //create the post request body
+        let body = {
+            "email": email,
+            "password": password
+        }
+        //create the config header file for request
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        axios.post('http://127.0.0.1:13200/api/signIn', body, config)
+            .then(response => {
+                setErrorMsg(false);
+                //store username, email and jwt token in redux store
+                dispatch(login({userName: "Julian Jaksch", email: email, token: response.data}));
+
+            })
+            .catch(function (error) {
+                if (error.response.status == 403) {
+                    setErrorMsg(true);
+                }
+            })
     }
 
     return (
@@ -17,11 +57,12 @@ function Login(props) {
                     <h1 className="login-form-header-heading">LOGIN</h1>
                 </div>
                 <div className="login-form-input">
-                    <input className="login-form-input-field" type="text" placeholder="Email" />
-                    <input className="login-form-input-field" type="text" placeholder="Password" />
+                    <input className="login-form-input-field" type="text" placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} />
+                    <input className="login-form-input-field" type="text" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
+                    <p className={errorMsg ? "login-form-errorMsg__active" : "login-form-errorMsg__inactive"}>Your email or password is wrong</p>
                 </div>
                 <div className="login-form-submit">
-                    <button className="login-form-submit-Btn">SIGN IN</button>
+                    <button className="login-form-submit-Btn" onClick={submitLogin}>SIGN IN</button>
                     <p className="login-form-submit-register">Don't have an account? <a className="login-form-submit-register-link" onClick={handleClickRegister}>Sign Up</a></p>
                 </div>
             </div>
