6a370c15445b4f953265540eda13b139f5d37c87
noctera <julian.jaksch@gmx.de>
2020-12-23 19:32:23 +0100

    added colors to palette

--- src/colors.js
+++ src/colors.js
@@ -2,8 +2,34 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   palette: {
-    primary: { main: "#F1F3FA" },
-    secondary: { main: "#313A46" },
+    primary: {
+      main: "#F1F3FA"
+    },
+    secondary: {
+      main: "#313A46"
+    },
+    third: {
+      main: "#242424",
+    },
+    action: {
+      main: "#727CF5"
+    },
+    shadow: {
+      main: "#b4bed6",
+      background: "hsl(0, 0%, 0%, 0.4)",
+    },
+    error: {
+      main: "#FF586E",
+      hover: "#dd4b5e"
+    },
+    success: {
+      main: "#0ACF97"
+    },
+    font: {
+      light: "#ffffff",
+      middle: "#8790A3",
+      dark: "#000000"
+    },
     
   }
 });
