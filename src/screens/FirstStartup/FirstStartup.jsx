a007a5713cd45232502ddf1c84b636030ece2566
Zicheng Wang <zicheng.wang@interweber.de>
2021-03-21 22:44:48 +0100

    chore: FirstStartup screen

--- src/screens/FirstStartup/FirstStartup.jsx
+++ src/screens/FirstStartup/FirstStartup.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Box } from "@material-ui/core";
 
-import AddLanguagePackage from "../AddLanguagePackage.jsx";
+import AddLanguagePackage from "../AddLanguagePackage/AddLanguagePackage.jsx";
 import "./FirstStartup.scss";
 
 function FirstStartup(props) {
   const isFirstLogin = useSelector((state) => state.login.firstLogin);
 
+  if (!isFirstLogin) {
+    return null;
+  }
+
   return (
-    <Box className={isFirstLogin ? "first-startup-visible" : "first-startup-invisible"}>
+    <Box className="first-startup-visible">
       <Box className="first-startup-inner">
         <h1 className="first-startup-inner-heading">CREATE YOUR FIRST VOCABULARY PACKAGE</h1>
         <AddLanguagePackage />
