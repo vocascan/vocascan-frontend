52f2fa40df49a8bc4d915a9aeab296682f73a3f0
Zicheng Wang <zicheng.wang@interweber.de>
2021-03-25 15:15:13 +0100

    fix top nav style

--- src/Components/Nav/TopNav.jsx
+++ src/Components/Nav/TopNav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
 import PersonIcon from "@material-ui/icons/Person";
@@ -6,17 +6,13 @@ import PersonIcon from "@material-ui/icons/Person";
 import Button from "@material-ui/core/Button";
 
 import { signOut } from "../../redux/Actions/login";
+import { NavLink } from "react-router-dom";
 
 import "./TopNav.scss";
 
 function TopNav() {
-  const [menuActive, setMenuActive] = useState(false);
   const dispatch = useDispatch();
   const { t } = useTranslation();
-
-  function handleClick() {
-    setMenuActive((value) => !value);
-  }
 
   //call redux store function to log out user
   function handleLogout() {
@@ -31,23 +27,27 @@ function TopNav() {
       <div className="title">
         <h1 className="text">{t("global.vocascan")}</h1>
       </div>
-      <h3 className="user" onClick={handleClick}>
+      <div className="user">
         <PersonIcon />
         {username || email}
-        <div className={menuActive ? "menu-active" : "menu-inactive"}>
+        <div className="menu">
           <ul className="menu-list">
             <li className="menu-list-item">
-              <Button className="menu-list-item-btn">{t("nav.profile")}</Button>
+              <NavLink to="/profile" className="menu-list-item-btn">
+                {t("nav.profile")}
+              </NavLink>
             </li>
             <li className="menu-list-item">
-              <Button className="menu-list-item-btn">{t("nav.settings")}</Button>
+              <NavLink to="/settings" className="menu-list-item-btn">
+                {t("nav.settings")}
+              </NavLink>
             </li>
             <li className="menu-list-item" onClick={handleLogout}>
               <Button className="menu-list-item-btn">{t("nav.logout")}</Button>
             </li>
           </ul>
         </div>
-      </h3>
+      </div>
     </div>
   );
 }
