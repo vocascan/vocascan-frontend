66f70eaba306c6faaa9cd7cc0503f03899970d58
Zicheng Wang <zicheng.wang@interweber.de>
2021-03-26 11:28:22 +0100

    add vocab add and remove translation

--- src/screens/AddVocab/AddVocab.jsx
+++ src/screens/AddVocab/AddVocab.jsx
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
+import uniqid from "uniqid";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import RemoveIcon from "@material-ui/icons/Remove";
 
 import Button from "../../Components/Button/Button";
 import TextInput from "../../Components/TextInput/TextInput";
 import Select from "../../Components/Select/Select";
+
 import "./AddVocab.scss";
 
 const AddVocab = () => {
@@ -16,13 +20,30 @@ const AddVocab = () => {
   const [groups] = useState([]);
   const [groupName, setGroupName] = useState("");
   const [foreignWord, setForeignWord] = useState("");
-  const [, setTranslations] = useState([]);
+  const [translations, setTranslations] = useState(() => [{ id: uniqid(), value: "" }]);
   const [description, setDescription] = useState("");
   const jwt = useSelector((state) => state.login.user.jwt);
   const serverAddress = useSelector((state) => state.login.serverAddress);
 
-  // default number of translation inputs to render
-  const translationInputs = [1, 2, 3];
+  const maxTranslations = 10;
+
+  const addTranslation = useCallback(() => {
+    if (translations.length >= maxTranslations) {
+      return;
+    }
+
+    setTranslations((trans) => [
+      ...trans,
+      {
+        id: uniqid(),
+        value: "",
+      },
+    ]);
+  }, [translations]);
+
+  const removeTranslation = useCallback((key) => {
+    setTranslations((trans) => trans.filter((elem) => elem.id !== key));
+  }, []);
 
   useEffect(() => {
     axios({
@@ -44,9 +65,11 @@ const AddVocab = () => {
   //   setGroups(vocascanModule.getGroups(packageName));
   // }
 
-  // function submit() {
-  //   vocascan.addVocab(packageName, groupName, foreignWord, translations, description);
-  // }
+  // const submit = useCallback(() => {
+  //   const submitTranslations = translations.map((elem) => elem.value);
+
+  //   vocascan.addVocab(packageName, groupName, foreignWord, submitTranslations, description);
+  // }, [packageName, groupName, foreignWord, translations, description]);
 
   // function addGroup(value) {
   //   vocascan.addGroup(value, packageName);
@@ -116,6 +139,7 @@ const AddVocab = () => {
         <div className="select-wrapper">
           <Select
             required
+            tabIndex={1}
             value={packageName}
             label={t("global.package")}
             options={languagePackageDropdownItems}
@@ -130,6 +154,7 @@ const AddVocab = () => {
         <div className="select-wrapper">
           <Select
             required
+            tabIndex={1}
             value={groupName}
             label={t("global.group")}
             options={groupDropdownItems}
@@ -143,28 +168,61 @@ const AddVocab = () => {
       <div className="input-fields">
         <TextInput
           required
+          tabIndex={1}
           placeholder={t("global.foreignWord")}
+          value={foreignWord}
           onChange={(value) => {
             setForeignWord(value);
           }}
         />
-        {translationInputs.map((elem, key) => (
-          <TextInput
-            required
-            key={elem}
-            placeholder={`${elem} ${t("global.translation")}`}
-            onChange={(value) => {
-              setTranslations((trans) => {
-                trans[key] = value;
-
-                return trans;
-              });
-            }}
-          />
-        ))}
+        {translations.map((elem, key) => {
+          return (
+            <div key={elem.id} className="translation-input-wrapper">
+              <TextInput
+                required
+                placeholder={`${key + 1}. ${t("global.translation")}`}
+                tabIndex={1}
+                onChange={(value) => {
+                  setTranslations((trans) => {
+                    return trans.map((element) =>
+                      element.id === elem.id
+                        ? {
+                            id: elem.id,
+                            value,
+                          }
+                        : element
+                    );
+                  });
+                }}
+              />
+              <Button
+                tabIndex={-1}
+                disabled={!key}
+                appearance="red"
+                variant="transparent"
+                onClick={() => removeTranslation(elem.id)}
+              >
+                <RemoveIcon />
+              </Button>
+            </div>
+          );
+        })}
+        <div className="add-translation-wrapper">
+          <Button
+            tabIndex={-1}
+            variant="transparent"
+            onClick={addTranslation}
+            disabled={translations.length >= maxTranslations}
+          >
+            <AddCircleOutlineIcon className="add-icon" />
+            {t("screens.addVocab.addTranslation")}
+          </Button>
+        </div>
         <TextInput
+          tabIndex={1}
           required
           placeholder={t("global.description")}
+          value={description}
           onChange={(value) => {
             setDescription(value);
           }}
@@ -174,6 +232,7 @@ const AddVocab = () => {
       <div className="form-submit">
         <Button
           block
+          tabIndex={-1}
           onClick={() => console.log("added")}
           disabled={!(foreignWord && setTranslations.length && description && groupName.length && packageName)}
         >
