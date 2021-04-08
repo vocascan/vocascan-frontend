bdb86243fa2722a921ccfb6f1679db8e62a29ef3
luwol03 <luwol03@gmail.com>
2021-04-08 11:15:05 +0200

    improved menu and added about dialog

---
+++ resources/utils/menu.js
@@ -0,0 +1,54 @@
+const { shell, ipcMain } = require("electron");
+const { ORGANIZATION_URL, DISCUSSION_URL, ISSUES_URL } = require("./constants");
+
+const SEPARATOR = { type: "separator" };
+
+const LINK_ENTRY = (label, url) => ({
+  label,
+  click: async () => {
+    await shell.openExternal(url);
+  },
+});
+
+const IPC_ENTRY = (label, event) => ({
+  label,
+  click: () => {
+    ipcMain.emit(event);
+  },
+});
+
+const isMac = process.platform === "darwin";
+
+const MENU_TEMPLATE = [
+  ...(isMac ? [{ role: "appMenu", label: "Vocascan" }] : []),
+  {
+    label: "View",
+    submenu: [
+      { role: "reload" },
+      { role: "forceReload" },
+      { role: "toggleDevTools" },
+      { type: "separator" },
+      { role: "resetZoom" },
+      { role: "zoomIn" },
+      { role: "zoomOut" },
+      { type: "separator" },
+      { role: "togglefullscreen" },
+    ],
+  },
+  {
+    label: "Help",
+    submenu: [
+      LINK_ENTRY("Learn More", ORGANIZATION_URL),
+      LINK_ENTRY("Community Discussions", DISCUSSION_URL),
+      LINK_ENTRY("Search Issues", ISSUES_URL),
+      SEPARATOR,
+      IPC_ENTRY("Check for updates", "checkForUpdates"),
+      SEPARATOR,
+      IPC_ENTRY("About", "openAbout"),
+    ],
+  },
+];
+
+module.exports = {
+  MENU_TEMPLATE,
+};
