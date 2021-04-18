494dc395891178cbbe9c720b4867d4eed476693c
noctera <julian.jaksch@gmx.de>
2021-04-18 22:50:56 +0200

    added SnackProvider and Modal

--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 
 import PersonIcon from "@material-ui/icons/Person";
 
 import Button from "../../Components/Button/Button.jsx";
+import Modal from "../../Components/Modal/Modal.jsx";
+import SnackbarContext from "../../context/SnackbarContext.jsx";
 
-import { getStats } from "../../utils/api.js";
+import { getStats, deleteUser } from "../../utils/api.js";
 
 import "./Profile.scss";
 
@@ -14,14 +16,20 @@ import Table from "../../Components/Table/Table";
 
 const Profile = () => {
   const { t } = useTranslation();
+  const { showSnack } = useContext(SnackbarContext);
+
   const username = useSelector((state) => state.login.user.username);
-  const [error, setError] = useState(false);
-  const [serverError, setServerError] = useState(false);
-
-  const serverAddress = useSelector((state) => state.login.serverAddress);
-  const selfHosted = useSelector((state) => state.login.selfHosted);
-
   const [stats, setStats] = useState({});
+
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const openDeleteModal = useCallback(() => {
+    setShowDeleteModal(true);
+  }, []);
+
+  const closeDeleteModal = useCallback(() => {
+    setShowDeleteModal(false);
+  }, []);
 
   useEffect(() => {
     getProfileStats();
@@ -31,19 +39,16 @@ const Profile = () => {
   const getProfileStats = () => {
     getStats()
       .then((response) => {
-        setError(false);
-
         //store stats in variables
         setStats(response.data);
       })
       .catch((event) => {
         if (event.response?.status === 401 || event.response?.status === 404) {
-          setServerError(false);
-          setError(true);
+          showSnack("error", "Error fetching stats");
           return;
         }
 
-        setServerError(true);
+        showSnack("error", "Internal Server Error");
       });
   };
 
@@ -66,7 +71,7 @@ const Profile = () => {
         accessor: "vocabs",
       },
     ],
-    []
+    [t]
   );
 
   const data = React.useMemo(
@@ -90,55 +95,77 @@ const Profile = () => {
         vocabs: stats.inactiveVocabulary,
       },
     ],
-    [stats]
+    [
+      stats.activeGroups,
+      stats.activeVocabulary,
+      stats.inactiveGroups,
+      stats.inactiveVocabulary,
+      stats.languagePackages,
+      t,
+    ]
   );
 
   return (
-    <div className="profile-screen">
-      <div className="profile-avatar-wrapper">
-        <PersonIcon className="profile-avatar" />
+    <>
+      <div className="profile-screen">
+        <div className="profile-avatar-wrapper">
+          <PersonIcon className="profile-avatar" />
+        </div>
+        <h1 className="profile-username">{username}</h1>
+        <Table columns={columns} data={data} pagination={false} />
+        <h1 className="account-settings-header">
+          {t("screens.profile.accountSettings.title")}
+        </h1>
+        <div className="account-settings">
+          <div className="account-settings-fields border-bottom">
+            <div className="description-wrapper">
+              <h3>{t("screens.profile.accountSettings.password.title")}</h3>
+              <p>{t("screens.profile.accountSettings.password.description")}</p>
+            </div>
+            <div className="button-wrapper">
+              <Button block uppercase appearance="red">
+                {t("screens.profile.accountSettings.password.title")}
+              </Button>
+            </div>
+          </div>
+          <div className="account-settings-fields border-bottom">
+            <div className="description-wrapper">
+              <h3>{t("screens.profile.accountSettings.email.title")}</h3>
+              <p>{t("screens.profile.accountSettings.email.description")}</p>
+            </div>
+            <div className="button-wrapper">
+              <Button block uppercase appearance="red">
+                {t("screens.profile.accountSettings.email.title")}
+              </Button>
+            </div>
+          </div>
+          <div className="account-settings-fields">
+            <div className="description-wrapper">
+              <h3>{t("screens.profile.accountSettings.delete.title")}</h3>
+              <p>{t("screens.profile.accountSettings.delete.description")}</p>
+            </div>
+            <div className="button-wrapper">
+              <Button
+                block
+                uppercase
+                appearance="red"
+                onClick={openDeleteModal}
+              >
+                {t("screens.profile.accountSettings.delete.title")}
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1 className="profile-username">{username}</h1>
-      <Table columns={columns} data={data} pagination={false} />
-      <h1 className="account-settings-header">
-        {t("screens.profile.accountSettings.title")}
-      </h1>
-      <div className="account-settings">
-        <div className="account-settings-fields border-bottom">
-          <div className="description-wrapper">
-            <h3>{t("screens.profile.accountSettings.password.title")}</h3>
-            <p>{t("screens.profile.accountSettings.password.description")}</p>
-          </div>
-          <div className="button-wrapper">
-            <Button block uppercase appearance="red">
-              {t("screens.profile.accountSettings.password.title")}
-            </Button>
-          </div>
-        </div>
-        <div className="account-settings-fields border-bottom">
-          <div className="description-wrapper">
-            <h3>{t("screens.profile.accountSettings.email.title")}</h3>
-            <p>{t("screens.profile.accountSettings.email.description")}</p>
-          </div>
-          <div className="button-wrapper">
-            <Button block uppercase appearance="red">
-              {t("screens.profile.accountSettings.email.title")}
-            </Button>
-          </div>
-        </div>
-        <div className="account-settings-fields">
-          <div className="description-wrapper">
-            <h3>{t("screens.profile.accountSettings.delete.title")}</h3>
-            <p>{t("screens.profile.accountSettings.delete.description")}</p>
-          </div>
-          <div className="button-wrapper">
-            <Button block uppercase appearance="red">
-              {t("screens.profile.accountSettings.delete.title")}
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Modal
+        title={"Delete Account"}
+        open={showDeleteModal}
+        onClose={closeDeleteModal}
+        closeOnClickOutside={false}
+      >
+        <h1 style={{ margin: "auto" }}>Hello World</h1>
+      </Modal>
+    </>
   );
 };
 
