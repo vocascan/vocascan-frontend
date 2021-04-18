910ef0df9a146e473237c562d6cd67e8345b1202
noctera <julian.jaksch@gmx.de>
2021-04-18 23:41:39 +0200

    linting

--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext, useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector, useDispatch } from "react-redux";
 
