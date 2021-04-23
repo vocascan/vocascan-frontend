276a72c1a7f36ce6bb88b8ed560e0de6d6527a4b
noctera <julian.jaksch@gmx.de>
2021-04-23 23:48:14 +0200

    added i18n

--- src/Components/PackageOverview/PackageOverview.jsx
+++ src/Components/PackageOverview/PackageOverview.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from "react";
+import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 
 import Button from "../Button/Button.jsx";
@@ -7,6 +8,7 @@ import "./PackageOverview.scss";
 
 const PackageOverview = ({ data }) => {
   const history = useHistory();
+  const { t } = useTranslation();
 
   const submitLearn = useCallback(() => history.push("/direction/"), [history]);
 
@@ -14,20 +16,24 @@ const PackageOverview = ({ data }) => {
     <div className="package-overview">
       <h1 className="package-heading">{data?.name}</h1>
       <p className="package-unresolved">
-        Total to learn: {data?.stats?.vocabularies?.unresolved}
+        {t("components.packageOverview.unresolved")}{" "}
+        {data?.stats?.vocabularies?.unresolved}
       </p>
-      <p className="package-today">Today: 100</p>
+      <p className="package-today">
+        {t("components.packageOverview.today")} 100
+      </p>
       <p className="package-unactivated">
-        Not activated: {data?.stats?.vocabularies?.unactivated}
+        {t("components.packageOverview.unactivated")}{" "}
+        {data?.stats?.vocabularies?.unactivated}
       </p>
       <div className="package-btn-wrapper">
         <Button block uppercase onClick={submitLearn}>
-          Learn
+          {t("global.learn")}
         </Button>
       </div>
       <div className="package-btn-wrapper">
         <Button variant={"outline"} uppercase block>
-          Activate
+          {t("global.activate")}
         </Button>
       </div>
     </div>
