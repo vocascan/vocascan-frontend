de59e9723daeee587378206dbaf70a8ae0da026e
noctera <julian.jaksch@gmx.de>
2021-04-24 19:37:10 +0200

    fixed i18n after merge conflict

--- src/screens/Learn/Direction/Direction.jsx
+++ src/screens/Learn/Direction/Direction.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 import DirectionBox from "../../../Components/DirectionBox/DirectionBox.jsx";
 
 import "./Direction.scss";
 
 const Direction = () => {
+  const { t } = useTranslation();
   return (
     <div className="direction">
-      <h1 className="box-title">Please choose your direction</h1>
+      <h1 className="box-title">{t("screens.direction.title")}</h1>
       <div className="box-wrapper">
         <DirectionBox />
         <DirectionBox direction="backwards" />
