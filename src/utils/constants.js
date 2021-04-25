6b5cfed091f815bfea8aab9f8d3130f2a7ada0b5
noctera <julian.jaksch@gmx.de>
2021-04-25 11:00:10 +0200

    improved flag rendering

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,112 +1,27 @@
 import ReactCountryFlag from "react-country-flag";
 
 export const languages = [
-  {
-    id: 0,
-    name: "Deutsch",
-    icon: (
-      <ReactCountryFlag
-        className="flag"
-        style={{
-          width: null,
-          height: null,
-        }}
-        countryCode="DE"
-        svg
-      />
-    ),
-  },
-  {
-    id: 1,
-    name: "Englisch",
-    icon: (
-      <ReactCountryFlag
-        className="flag"
-        style={{
-          width: null,
-          height: null,
-        }}
-        countryCode="GB"
-        svg
-      />
-    ),
-  },
-  {
-    id: 2,
-    name: "Spanisch",
-    icon: (
-      <ReactCountryFlag
-        className="flag"
-        style={{
-          width: null,
-          height: null,
-        }}
-        countryCode="ES"
-        svg
-      />
-    ),
-  },
-  {
-    id: 3,
-    name: "Portugisisch",
-    icon: (
-      <ReactCountryFlag
-        className="flag"
-        style={{
-          width: null,
-          height: null,
-        }}
-        countryCode="PT"
-        svg
-      />
-    ),
-  },
-  {
-    id: 4,
-    name: "Türkisch",
-    icon: (
-      <ReactCountryFlag
-        className="flag"
-        style={{
-          width: null,
-          height: null,
-        }}
-        countryCode="TR"
-        svg
-      />
-    ),
-  },
-  {
-    id: 5,
-    name: "Russisch",
-    icon: (
-      <ReactCountryFlag
-        className="flag"
-        style={{
-          width: null,
-          height: null,
-        }}
-        countryCode="RU"
-        svg
-      />
-    ),
-  },
-  {
-    id: 6,
-    name: "Französisch",
-    icon: (
-      <ReactCountryFlag
-        className="flag"
-        style={{
-          width: null,
-          height: null,
-        }}
-        countryCode="FR"
-        svg
-      />
-    ),
-  },
-];
+  { name: "Deutsch", countryCode: "DE" },
+  { name: "Englisch", countryCode: "GB" },
+  { name: "Spanisch", countryCode: "ES" },
+  { name: "Portugisisch", countryCode: "PT" },
+  { name: "Türkisch", countryCode: "TR" },
+  { name: "Russisch", countryCode: "RU" },
+  { name: "Französisch", countryCode: "FR" },
+].map((language) => ({
+  ...language,
+  icon: (
+    <ReactCountryFlag
+      className="flag"
+      style={{
+        width: null,
+        height: null,
+      }}
+      countryCode={language.countryCode}
+      svg
+    />
+  ),
+}));
 
 export const vocascanServer = "https://vocascan.com"; // TODO: add domain later
 
