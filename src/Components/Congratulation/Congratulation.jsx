07cbc081499458095af33d746821990b279b8cfa
noctera <julian.jaksch@gmx.de>
2021-05-16 11:05:47 +0200

    cleaned up code

--- src/Components/Congratulation/Congratulation.jsx
+++ src/Components/Congratulation/Congratulation.jsx
@@ -9,17 +9,18 @@ const Congratulation = ({ percentage }) => {
   const { t } = useTranslation();
   const [congratulation, setCongratulations] = useState();
 
+  const translations = t("screens.endScreen.congratulations", {
+    returnObjects: true,
+  });
+
   useEffect(() => {
     //get size of congratulation array and calculate the message with the given percentage
-    const translations = t("screens.endScreen.congratulations", {
-      returnObjects: true,
-    });
     setCongratulations(
       translations[
         scaleValue(percentage, [0, 100], [1, translations.length - 1])
       ]
     );
-  }, [percentage, t]);
+  }, [percentage, t, translations]);
   return <p className="congratulation">{congratulation}</p>;
 };
 
