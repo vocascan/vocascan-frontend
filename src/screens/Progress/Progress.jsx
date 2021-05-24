bc763b3593dff6574fca672f09c6871aa658ecf5
luwol03 <luwol03@gmail.com>
2021-05-24 10:43:59 +0200

    improved i18n

--- src/screens/Progress/Progress.jsx
+++ src/screens/Progress/Progress.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 import "./Progress.scss";
 
 const Progress = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="progress">
       <div>
-        <h1>Progress</h1>
-        <h3>Coming soon...</h3>
+        <h1>{t("screens.progress.title")}</h1>
+        <h3>{t("global.comingSoon")}</h3>
       </div>
     </div>
   );
