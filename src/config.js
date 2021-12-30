a06d0640dd27e2242050e75fc68b7271167ed1d2
luwol03 <luwol03@gmail.com>
2021-12-30 17:39:53 +0100

    added BASE_URL and SHOW_PLANS options

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 window.VOCASCAN_CONFIG = {
   // inject default config options
   ENV: "web",
+  BASE_URL: "",
+  SHOW_PLANS: undefined,
 
   // inject build variables
   ...Object.entries(process.env).reduce((acc, [key, value]) => {
@@ -14,3 +16,8 @@ window.VOCASCAN_CONFIG = {
   // inject already existing config options
   ...window.VOCASCAN_CONFIG,
 };
+
+// if SHOW_PLANS is not configured but a BASE_URL is configured -> dont show the plans
+if (window.VOCASCAN_CONFIG.SHOW_PLANS === undefined) {
+  window.VOCASCAN_CONFIG.SHOW_PLANS = !window.VOCASCAN_CONFIG.BASE_URL;
+}
