aeae2629f5773dd61b6ce49a49c97b7716e7e1df
luwol03 <luwol03@gmail.com>
2022-01-02 21:57:13 +0100

    extended useFeature hook with env check

--- src/hooks/useFeature.js
+++ src/hooks/useFeature.js
@@ -3,29 +3,37 @@ import { useSelector } from "react-redux";
 import { gte } from "semver";
 
 export const FEATURES = {
-  IMPORT_EXPORT: "1.1.0",
+  IMPORT_EXPORT: { minVersion: "1.1.0", env: "electron" },
 };
 
 const useFeature = (feature) => {
   const serverInfo = useSelector((state) => state.login.serverInfo);
   const isSupported = useMemo(() => {
-    // in case that serverVersion is not yet defined
-    if (!serverInfo?.version) {
+    return Object.entries(feature).every(([key, value]) => {
+      if (key === "minVersion") {
+        // in case that serverVersion is not yet defined
+        if (!serverInfo?.version) {
+          return false;
+        }
+
+        // compare semver versions
+        if (gte(serverInfo?.version, value)) {
+          return true;
+        }
+
+        // make the feature always in development versions available to allow testing
+        if (process.env.NODE_ENV === "development") {
+          return true;
+        }
+
+        // feature not supported
+        return false;
+      } else if (key === "env") {
+        return window.VOCASCAN_CONFIG.ENV === value;
+      }
+
       return false;
-    }
-
-    // compare semver versions
-    if (gte(serverInfo?.version, feature)) {
-      return true;
-    }
-
-    // make the feature always in development versions available to allow testing
-    if (process.env.NODE_ENV === "development") {
-      return true;
-    }
-
-    // feature not supported
-    return false;
+    });
   }, [feature, serverInfo?.version]);
 
   return {
