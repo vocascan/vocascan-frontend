3137a0a279baaff050b589a324c0d653393cd973
noctera <noctera.dev@gmail.com>
2022-01-14 15:54:22 +0100

    fix: i18n integration

--- src/Components/CookieConsentBanner/CookieConsentBanner.jsx
+++ src/Components/CookieConsentBanner/CookieConsentBanner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CookieConsent from "react-cookie-consent";
+import { useTranslation } from "react-i18next";
 
 import { pages } from "../../utils/constants.js";
 
@@ -8,25 +9,27 @@ import "./CookieConsentBanner.scss";
 import useLinkCreator from "../../hooks/useLinkCreator";
 
 const CookieConsentBanner = () => {
+  const { t } = useTranslation();
+
   const { isValid: isPrivacyPolicyValid, url: privacyPolicyUrl } =
     useLinkCreator({ path: pages.privacyPolicy });
 
   return (
     <>
-      {isPrivacyPolicyValid && (
+      {isPrivacyPolicyValid && window.VOCASCAN_CONFIG.ENV === "web" && (
         <CookieConsent
-          buttonText="I understand"
+          buttonText={t("components.cookieConsent.accept")}
           cookieName="redux-strings"
           disableStyles
         >
-          This website uses cookies to enhance the user experience.{" "}
+          {t("components.cookieConsent.text")}
           <a
             className="cookie-consent-link"
             href={privacyPolicyUrl}
             target="_blank"
             rel="noreferrer"
           >
-            learn more
+            {t("components.cookieConsent.learnMore")}
           </a>
         </CookieConsent>
       )}
