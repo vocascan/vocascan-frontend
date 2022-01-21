f893f58976946f2e7b31a6631a50bd2fe8ab89a7
noctera <noctera.dev@gmail.com>
2022-01-21 18:06:29 +0100

    fix: removed fancy menu style

--- src/redux/Reducers/setting.js
+++ src/redux/Reducers/setting.js
@@ -1,9 +1,8 @@
 import { defineState } from "redux-localstore";
 
-import { SET_LANGUAGE, SET_MENU_STYLE } from "../Actions/index.js";
+import { SET_LANGUAGE } from "../Actions/index.js";
 
 const defaultState = {
-  menuStyle: "default", // fancy, default
   language: "en",
 };
 
@@ -11,12 +10,6 @@ const initialState = defineState(defaultState)("setting");
 
 const loginReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_MENU_STYLE:
-      return {
-        ...state,
-        menuStyle: action.payload.menuStyle,
-      };
-
     case SET_LANGUAGE:
       return {
         ...state,
--- src/screens/Settings/Settings.jsx
+++ src/screens/Settings/Settings.jsx
@@ -1,14 +1,12 @@
 import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import Button from "../../Components/Button/Button.jsx";
-import Switch from "../../Components/Form/Switch/Switch.jsx";
 import LanguageSelector from "../../Components/LanguageSelector/LanguageSelector.jsx";
 
 import VersionTable from "../../Components/VersionTable/VersionTable.js";
 import { openGuide } from "../../redux/Actions/login.js";
-import { setMenuStyle } from "../../redux/Actions/setting.js";
 
 import "./Settings.scss";
 
@@ -16,13 +14,6 @@ const Settings = () => {
   const { t } = useTranslation();
 
   const dispatch = useDispatch();
-  const menuStyle = useSelector((state) => state.setting.menuStyle);
-
-  const onChangeMenu = useCallback(() => {
-    dispatch(
-      setMenuStyle({ menuStyle: menuStyle === "default" ? "fancy" : "default" })
-    );
-  }, [dispatch, menuStyle]);
 
   const reopenGuide = useCallback(() => {
     dispatch(openGuide());
@@ -31,14 +22,6 @@ const Settings = () => {
   return (
     <div className="settings-wrapper">
       <h1 className="heading">{t("screens.settings.title")}</h1>
-      <Switch
-        switcher
-        label={t("screens.settings.menu.label")}
-        optionLeft={t("screens.settings.menu.optionLeft")}
-        optionRight={t("screens.settings.menu.optionRight")}
-        onChange={onChangeMenu}
-        checked={menuStyle !== "default"}
-      />
 
       <LanguageSelector />
 
