f0e5ea96028f67adc21cedd0d2b548a73743d46a
luwol03 <luwol03@gmail.com>
2022-01-23 11:30:01 +0100

    fixed flags in firefox

--- src/Components/Flag/Flag.jsx
+++ src/Components/Flag/Flag.jsx
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { useEffect } from "react";
 
-import { countryFlags, languageCountryMap } from "./language-country-map.js";
+import {
+  languageCountryMap,
+  spriteSheetPositions,
+} from "./language-country-map.js";
 
 import "./Flag.scss";
 
-const Flag = ({ languageCode, border = false, size = "small" }) => {
-  let code = null;
+const sizeMap = {
+  small: 0.13,
+  medium: 0.3,
+  large: 0.6,
+};
 
-  if (countryFlags.includes(languageCode)) {
-    code = languageCode;
-  } else if (languageCountryMap[languageCode]) {
-    code = languageCountryMap[languageCode];
-  }
+const Flag = ({ languageCode, border = false, size = "small", scale }) => {
+  const [computedScale, setComputedScale] = useState(0);
+
+  useEffect(() => {
+    if (scale) {
+      setComputedScale(scale);
+    } else if (size && sizeMap[size]) {
+      setComputedScale(sizeMap[size]);
+    } else {
+      setComputedScale(sizeMap.small);
+    }
+  }, [scale, size]);
+
+  const style = useMemo(() => {
+    let code = "unknown";
+
+    if (Object.keys(spriteSheetPositions).includes(languageCode)) {
+      code = languageCode;
+    } else if (languageCountryMap[languageCode]) {
+      code = languageCountryMap[languageCode];
+    }
+
+    return {
+      width: 200 * computedScale,
+      height: 150 * computedScale,
+      backgroundSize: `${3300 * computedScale}px ${3060 * computedScale}px`,
+      backgroundPosition: spriteSheetPositions[code]
+        .map((x) => `${x * computedScale}px`)
+        .join(" "),
+      borderRadius: `${30 * computedScale}px`,
+      boxShadow: border ? `0 0 0 ${10 * computedScale}px` : "none",
+    };
+  }, [border, computedScale, languageCode]);
 
   return (
-    <span
-      className={`flag flag-${code || "unknown"} ${
-        border ? "bordered" : ""
-      } ${size}`}
-    />
+    <div className="flag">
+      <span style={style} />
+    </div>
   );
 };
 
