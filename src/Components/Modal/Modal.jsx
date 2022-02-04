b62a87b77a78be443208d7e1dc612096a1704fd9
noctera <noctera.dev@gmail.com>
2022-02-04 23:46:19 +0100

    refactor: modal tailwindcss

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,10 +1,9 @@
+import clsx from "clsx";
 import React, { useCallback, useEffect, useRef } from "react";
 
 import CloseIcon from "@material-ui/icons/Close";
 
 import Button from "../Button/Button.jsx";
-
-import "./Modal.scss";
 
 const Modal = ({
   title,
@@ -17,6 +16,13 @@ const Modal = ({
   children,
 }) => {
   const ref = useRef(null);
+
+  const modalLayoutClasses = clsx(
+    size === "" && "w-3/5 h-3/5",
+    size === "small" && "w-2/4 max-w-lg h-auto",
+    size === "large" && "w-4/5 h-4/5",
+    size === "maxed" && "w-full h-screen"
+  );
 
   const escapeListener = useCallback(
     (e) => {
@@ -66,20 +72,23 @@ const Modal = ({
   }
 
   return (
-    <div className="modal">
-      <div className={`inner ${size ? size : ""}`} ref={ref}>
+    <div className="w-full h-screen bg-background-modal z-50 flex justify-center absolute left-0 top-0">
+      <div
+        className={`relative m-auto bg-white z-11 py-10 px-16 flex flex-col justify-between items-center border rounded overflow-y-auto ${modalLayoutClasses}`}
+        ref={ref}
+      >
         {renderClose && (
           <Button
             tabIndex={-1}
             appearance="primary"
             variant="transparent"
             onClick={onClose}
-            className="close"
+            className="bg-primary-standard absolute top-5 right-5 rounded-full p-2.5 hover:bg-primary-dark cursor-pointer"
           >
             <CloseIcon />
           </Button>
         )}
-        {title && <h1 className="heading">{title}</h1>}
+        {title && <h1 className="uppercase text-2xl mt-5">{title}</h1>}
         {children}
       </div>
     </div>
