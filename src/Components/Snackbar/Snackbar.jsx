7214c368ffa22756bf6dc44214081175ec9998e5
noctera <noctera.dev@gmail.com>
2022-02-04 23:47:33 +0100

    refactor: snackbar tailwindcss

--- src/Components/Snackbar/Snackbar.jsx
+++ src/Components/Snackbar/Snackbar.jsx
@@ -9,14 +9,18 @@ const Snackbar = () => {
   const { snack } = useContext(SnackbarContext);
 
   const classes = clsx(
-    "snackbar",
-    `snackbar-${snack.variant}`,
-    snack.show && "show"
+    snack.variant === "success" && "bg-green-light text-green-dark",
+    snack.variant === "error" && "bg-red-light text-red-dark",
+    snack.variant === "info" && "bg-primary-light text-primary-dark-standard",
+    snack.show && "select-none opacity-1 transition-all ease-in duration-400",
+    !snack.show && "opacity-0"
   );
 
   return (
-    <div className={classes}>
-      <span className="snackbar-text">{snack.text}</span>
+    <div
+      className={`w-72 absolute bottom-4 flex left-0 right-0 ml-auto mr-auto justify-center py-2.5 px-5 rounded-lg z-50 transition-all ease-in duration-400 ${classes} md:left-auto md:right-4 md:ml-0 md:mr-0`}
+    >
+      <span className="font-size text-base font-normal">{snack.text}</span>
     </div>
   );
 };
