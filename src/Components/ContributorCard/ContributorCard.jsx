aa424c238400369a71c93e3340aa7500457ed2c2
noctera <noctera.dev@gmail.com>
2022-04-13 23:23:30 +0200

    fix: contributor card hover

--- src/Components/ContributorCard/ContributorCard.jsx
+++ src/Components/ContributorCard/ContributorCard.jsx
@@ -5,8 +5,8 @@ import PersonIcon from "@material-ui/icons/Person";
 const Card = ({ name, url, imageUrl, description }) => {
   return (
     <a href={url ? url : `/#/about`} target="_blank" rel="noreferrer noopener">
-      <div className="w-32 h-44 m-2 inline-block relative rounded-lg overflow-hidden">
-        <div className="group w-full absolute bottom-0 top-32 bg-background-inverse text-white flex flex-col justify-center items-center ease-in-out duration-150 hover:top-0 hover:bg-transparent hover:bg-gradient-to-t hover:from-background-inverse hover:via-background-inverse">
+      <div className="group w-32 h-44 m-2 inline-block relative rounded-lg overflow-hidden">
+        <div className="w-full absolute bottom-0 top-32 bg-background-inverse text-white flex flex-col justify-center items-center ease-in-out duration-150 group-hover:top-0 group-hover:bg-transparent group-hover:bg-gradient-to-t group-hover:from-background-inverse group-hover:via-background-inverse">
           <div className="text-sm">{name}</div>
           <p className="text-grey text-xs mt-1 whitespace-pre-wrap leading-none hidden group-hover:block">
             {description}
