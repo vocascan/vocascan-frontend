5c4b6445ae07b1e15d586fd3ed10d226f4ba0ec1
noctera <noctera.dev@gmail.com>
2022-04-13 23:19:52 +0200

    refactor: updated ContributorCard to tailwindcss

--- src/Components/ContributorCard/ContributorCard.jsx
+++ src/Components/ContributorCard/ContributorCard.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 import PersonIcon from "@material-ui/icons/Person";
 
-import "./ContributorCard.scss";
-
 const Card = ({ name, url, imageUrl, description }) => {
   return (
     <a href={url ? url : `/#/about`} target="_blank" rel="noreferrer noopener">
-      <div className="card outline">
-        <div className="backdrop">
-          <div className="name">{name}</div>
-          <p className="description">{description}</p>
+      <div className="w-32 h-44 m-2 inline-block relative rounded-lg overflow-hidden">
+        <div className="group w-full absolute bottom-0 top-32 bg-background-inverse text-white flex flex-col justify-center items-center ease-in-out duration-150 hover:top-0 hover:bg-transparent hover:bg-gradient-to-t hover:from-background-inverse hover:via-background-inverse">
+          <div className="text-sm">{name}</div>
+          <p className="text-grey text-xs mt-1 whitespace-pre-wrap leading-none hidden group-hover:block">
+            {description}
+          </p>
         </div>
         {imageUrl ? (
-          <img src={imageUrl} alt={name} />
+          <img className="w-32" src={imageUrl} alt={name} />
         ) : (
-          <div className="placeholderimage">
+          <div className="h-2/3 flex justify-center items-center">
             <PersonIcon style={{ fontSize: 70 }} />
           </div>
         )}
