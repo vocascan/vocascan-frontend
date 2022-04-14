096acfa398df84bcb1ca24309edb032086a330ff
noctera <noctera.dev@gmail.com>
2022-04-14 16:51:45 +0200

    refactor: updated Switch to Tailwindcss

--- src/Components/ContributorCard/ContributorCard.jsx
+++ src/Components/ContributorCard/ContributorCard.jsx
@@ -6,7 +6,7 @@ const Card = ({ name, url, imageUrl, description }) => {
   return (
     <a href={url ? url : `/#/about`} target="_blank" rel="noreferrer noopener">
       <div className="group w-32 h-44 m-2 inline-block relative rounded-lg overflow-hidden">
-        <div className="w-full absolute bottom-0 top-32 bg-background-inverse text-white flex flex-col justify-center items-center ease-in-out duration-150 group-hover:top-0 group-hover:bg-transparent group-hover:bg-gradient-to-t group-hover:from-background-inverse group-hover:via-background-inverse">
+        <div className="w-full absolute bottom-0 top-32 bg-background-inverse text-white flex flex-col justify-center items-center ease-in-out duration-150 group-hover:top-0 group-hover:bg-transparent group-hover:bg-gradient-to-t group-hover:from-background-inverse group-hover:via-background-inverse group-hover:to-background-inverse/50">
           <div className="text-sm">{name}</div>
           <p className="text-grey text-xs mt-1 whitespace-pre-wrap leading-none hidden group-hover:block">
             {description}
--- src/Components/Form/Switch/Switch.jsx
+++ src/Components/Form/Switch/Switch.jsx
@@ -1,10 +1,9 @@
+import clsx from "clsx";
 import React from "react";
 
 import InfoIcon from "@material-ui/icons/Info";
 
 import Tooltip from "../../Tooltip/Tooltip.jsx";
-
-import "./Switch.scss";
 
 const Switch = ({
   disabled = false,
@@ -18,46 +17,64 @@ const Switch = ({
   label = null,
   appearance = "default",
 }) => {
+  const styleClass = clsx(
+    checked &&
+      appearance === "default" &&
+      "bg-primary-standard before:translate-x-5",
+    checked &&
+      appearance === "on-off" &&
+      "bg-green-standard before:translate-x-5",
+    !checked && appearance === "on-off" && "bg-red-standard",
+    !checked && appearance === "default" && "bg-background-inverse"
+  );
+
   return (
-    <div className="switch-wrapper">
-      {label && <label className="label-top">{label}</label>}
+    <div className="flex flex-col justify-start items-start my-4 mx-0">
+      {label && <label className="mb-2 font-normal">{label}</label>}
       <div
-        className={`switch-wrapper-inner ${
-          switcher && optionRight ? "switcher-left" : ""
+        className={`w-full flex items-center justify-between ${
+          switcher && optionRight ? "justify-start" : ""
         }`}
       >
         {optionLeft && (
-          <div className="label-wrapper">
-            <label className="label-left">{optionLeft}</label>
+          <div className="flex justify-center items-center">
+            <label className="mr-5">{optionLeft}</label>
             {infoLeft && (
               <InfoIcon
                 tabIndex={-1}
                 data-tip={infoLeft}
                 data-for="switch-tooltip"
-                className="info-sign"
+                className="text-primary-light my-0 -mx-4 flex items-center relative hover:cursor-help"
               />
             )}
           </div>
         )}
-        <label className={`switch ${disabled ? "disabled" : ""}`}>
+        <label
+          className={`w-12 h-7 relative inline-block ${
+            disabled ? "opacity-30 pointer-events-none" : ""
+          }`}
+        >
           <input
+            className="opacity-0 w-0 h-0"
             tabIndex={-1}
             type="checkbox"
             checked={checked}
             onChange={onChange}
           />
-          <span className={`slider slider-${appearance}`}></span>
+          <span
+            className={`absolute top-0 left-0 right-0 bottom-0 cursor-pointer transition-all rounded-3xl before:rounded-[50%] before:absolute before:h-5 before:w-5 before:left-1 before:bottom-1 before:bg-white before:transition-all ${styleClass}`}
+          ></span>
         </label>
         {switcher && optionRight && (
-          <div className="label-wrapper">
+          <div className="flex justify-center items-center">
             {infoRight && (
               <InfoIcon
                 data-tip={infoRight}
                 data-for="switch-tooltip"
-                className="info-sign"
+                className="text-primary-light my-0 -mx-4 flex items-center relative hover:cursor-help"
               />
             )}
-            <label className="label-right">{optionRight}</label>
+            <label className="ml-5">{optionRight}</label>
           </div>
         )}
       </div>
