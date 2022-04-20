f6a76456efd3861729ced69d82525838e96cbd3b
Tracer1337 <merlin.moelter@gmail.com>
2022-04-20 19:52:58 +0200

    refactor: further config generation refactoring

--- docker/generate-config.js
+++ docker/generate-config.js
@@ -1,19 +1,53 @@
 const fs = require("fs");
+const path = require("path");
 
-const configPath = "/usr/share/nginx/html/config.js";
+const htmlPath =
+  process.env.NODE_ENV === "development"
+    ? path.resolve(__dirname, "..", "public")
+    : "/usr/share/nginx/html";
+const configPath = path.resolve(htmlPath, "config.js");
 
 console.log("Pre-start: Generating config file…");
 
-if (!fs.existsSync(configPath)) {
-  console.warn(
-    `Pre-start: ⚠️ No existing config file found under "${configPath}".`
+const config = readConfig();
+
+Object.entries(process.env).forEach(([key, value]) => {
+  if (key.startsWith("VOCASCAN_")) {
+    config[key.replace(/^VOCASCAN_/, "")] = convertToNativeType(value);
+  }
+});
+
+writeConfig(config);
+
+console.log(
+  `Pre-start: ✓ Successfully written config file to "${configPath}".`
+);
+
+function readConfig() {
+  if (!fs.existsSync(configPath)) {
+    console.warn(
+      `Pre-start: ⚠️ No existing config file found under "${configPath}".`
+    );
+    process.exit();
+  }
+  const content = fs.readFileSync(configPath, "utf-8");
+  const existing = content.match(
+    /window\.VOCASCAN_CONFIG\s*=\s*JSON.parse\(`([\s\S]*)`\)/
   );
-  process.exit();
+  if (!existing || existing.length < 2) {
+    console.warn("Could not find existing config in config.js");
+    process.exit();
+  }
+  return JSON.parse(existing[1]);
 }
 
-const content = fs.readFileSync(configPath, "utf-8");
+function writeConfig(config) {
+  const json = JSON.stringify(config, null, 4);
+  const newContent = `window.VOCASCAN_CONFIG = JSON.parse(\`${json}\`);\n`;
+  fs.writeFileSync(configPath, newContent, "utf-8");
+}
 
-const convertToNativeType = (input) => {
+function convertToNativeType(input) {
   if (["true", "false"].includes(input)) {
     return input === "true";
   }
@@ -23,30 +57,4 @@ const convertToNativeType = (input) => {
   }
 
   return input;
-};
-
-const envVars = Object.entries(process.env).reduce((acc, [key, value]) => {
-  if (key.startsWith("VOCASCAN_")) {
-    acc[key.replace(/^VOCASCAN_/, "")] = convertToNativeType(value);
-  }
-
-  return acc;
-}, {});
-
-const existing = content.match(
-  /window\.VOCASCAN_CONFIG\s*=\s*JSON.parse\(`([\s\S]*)`\)/
-);
-if (!existing || existing.length < 2) {
-  console.warn("Could not find existing config in config.js");
-  process.exit();
 }
-
-const existingConfig = JSON.parse(existing[1]);
-const newJSON = JSON.stringify({ ...existingConfig, ...envVars }, null, 2);
-const newContent = `window.VOCASCAN_CONFIG = JSON.parse(\`${newJSON}\`);\n`;
-
-fs.writeFileSync(configPath, newContent, { encoding: "utf-8", flag: "w" });
-
-console.log(
-  `Pre-start: ✓ Successfully written config file to "${configPath}".`
-);
