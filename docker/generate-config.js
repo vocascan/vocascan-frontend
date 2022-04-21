02be3b150d36d9c88002629a7e4355e028ebb80b
Tracer1337 <merlin.moelter@gmail.com>
2022-04-21 19:55:53 +0200

    refactor: hoist function definitions in generate-config script

--- docker/generate-config.js
+++ docker/generate-config.js
@@ -1,6 +1,42 @@
 const fs = require("fs");
 const path = require("path");
 
+const readConfig = () => {
+  if (!fs.existsSync(configPath)) {
+    console.warn(
+      `Pre-start: ⚠️ No existing config file found under "${configPath}".`
+    );
+    process.exit();
+  }
+  const content = fs.readFileSync(configPath, "utf-8");
+  const existing = content.match(
+    /window\.VOCASCAN_CONFIG\s*=\s*JSON.parse\(`([\s\S]*)`\)/
+  );
+  if (!existing || existing.length < 2) {
+    console.warn("Could not find existing config in config.js");
+    process.exit();
+  }
+  return JSON.parse(existing[1]);
+};
+
+const writeConfig = (config) => {
+  const json = JSON.stringify(config, null, 4);
+  const newContent = `window.VOCASCAN_CONFIG = JSON.parse(\`${json}\`);\n`;
+  fs.writeFileSync(configPath, newContent, "utf-8");
+};
+
+const convertToNativeType = (input) => {
+  if (["true", "false"].includes(input)) {
+    return input === "true";
+  }
+
+  if (!Number.isNaN(parseInt(input))) {
+    return parseInt(input);
+  }
+
+  return input;
+};
+
 const htmlPath =
   process.env.NODE_ENV === "development"
     ? path.resolve(__dirname, "..", "public")
@@ -35,39 +71,3 @@ writeConfig(config);
 console.log(
   `Pre-start: ✓ Successfully written config file to "${configPath}".`
 );
-
-function readConfig() {
-  if (!fs.existsSync(configPath)) {
-    console.warn(
-      `Pre-start: ⚠️ No existing config file found under "${configPath}".`
-    );
-    process.exit();
-  }
-  const content = fs.readFileSync(configPath, "utf-8");
-  const existing = content.match(
-    /window\.VOCASCAN_CONFIG\s*=\s*JSON.parse\(`([\s\S]*)`\)/
-  );
-  if (!existing || existing.length < 2) {
-    console.warn("Could not find existing config in config.js");
-    process.exit();
-  }
-  return JSON.parse(existing[1]);
-}
-
-function writeConfig(config) {
-  const json = JSON.stringify(config, null, 4);
-  const newContent = `window.VOCASCAN_CONFIG = JSON.parse(\`${json}\`);\n`;
-  fs.writeFileSync(configPath, newContent, "utf-8");
-}
-
-function convertToNativeType(input) {
-  if (["true", "false"].includes(input)) {
-    return input === "true";
-  }
-
-  if (!Number.isNaN(parseInt(input))) {
-    return parseInt(input);
-  }
-
-  return input;
-}
