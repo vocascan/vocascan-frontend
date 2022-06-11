e8465af86fbba72ab71ad2c13004bec0512754ad
noctera <noctera.dev@gmail.com>
2022-06-11 13:53:51 +0200

    fix: contributor card structure

--- src/Components/ContributorCard/ContributorCard.jsx
+++ src/Components/ContributorCard/ContributorCard.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 
+import PersonIcon from "@material-ui/icons/Person";
+
 import "./ContributorCard.scss";
 
 const Card = ({ name, url, imageUrl, description }) => {
   return (
-    <a
-      href={url ? url : `https://github.com/${name}`}
-      target="_blank"
-      rel="noreferrer noopener"
-    >
+    <a href={url ? url : `/#/about`} target="_blank" rel="noreferrer noopener">
       <div className="card outline">
         <div className="backdrop">
           <div className="name">{name}</div>
           <div className="description">{description}</div>
         </div>
-        <img
-          src={imageUrl ? imageUrl : `https://github.com/${name}.png`}
-          alt={name}
-        />
+        {imageUrl ? (
+          <img className="card-image" src={imageUrl} alt={name} />
+        ) : (
+          <div className="card-image-bp">
+            <PersonIcon style={{ transform: "scale(3.5)", color: "#000" }} />
+          </div>
+        )}
       </div>
     </a>
   );
