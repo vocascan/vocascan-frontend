7497f9e1be8d61ad64523df9ad303e3837ec5e24
noctera <noctera.dev@gmail.com>
2022-06-11 21:28:25 +0200

    fix: code review

--- src/Components/ContributorCard/ContributorCard.jsx
+++ src/Components/ContributorCard/ContributorCard.jsx
@@ -6,7 +6,7 @@ import "./ContributorCard.scss";
 
 const Card = ({ name, url, imageUrl, description }) => {
   return (
-    <a href={url ? url : `/#/about`} target="_blank" rel="noreferrer noopener">
+    <a href={url || `/#/about`} target="_blank" rel="noreferrer noopener">
       <div className="card outline">
         <div className="backdrop">
           <div className="name">{name}</div>
@@ -16,7 +16,7 @@ const Card = ({ name, url, imageUrl, description }) => {
           <img className="card-image" src={imageUrl} alt={name} />
         ) : (
           <div className="card-image-bp">
-            <PersonIcon style={{ transform: "scale(3.5)", color: "#000" }} />
+            <PersonIcon className="card-image-bp-icon" />
           </div>
         )}
       </div>
