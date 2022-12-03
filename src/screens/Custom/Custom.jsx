dd0a05673cbf8388a8d3fdfc453afc715043231b
noctera <noctera.dev@gmail.com>
2022-12-03 19:18:34 +0100

    style: custom learning page table

--- src/screens/Custom/Custom.jsx
+++ src/screens/Custom/Custom.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 import Switch from "../../Components/Form/Switch/Switch.jsx";
+import Table from "../../Components/Table/Table.jsx";
+import Tooltip from "../../Components/Tooltip/Tooltip.jsx";
 
 import { clearQuery } from "../../redux/Actions/query.js";
 import { setLearnedPackage } from "../../redux/Actions/query.js";
@@ -13,48 +15,10 @@ import "./Custom.scss";
 import Button from "../../Components/Button/Button";
 import { getPackages } from "../../utils/api";
 
-const CustomLearningPackageOverview = ({ data, onlyActivated }) => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-
-  const selectPackage = useCallback(() => {
-    dispatch(clearQuery());
-    dispatch(
-      setLearnedPackage({
-        foreignWordLanguage: data.foreignWordLanguage,
-        translatedWordLanguage: data.translatedWordLanguage,
-        //using fixed value until server gives us this property
-        languagePackageId: data.id,
-        groupIds: [],
-        vocabsToday: 0,
-        staged: false,
-        onlyActivated: onlyActivated,
-        customLearning: true,
-      })
-    );
-
-    history.push("/learn/selection/custom/");
-  }, [
-    data.foreignWordLanguage,
-    data.id,
-    data.translatedWordLanguage,
-    dispatch,
-    history,
-    onlyActivated,
-  ]);
-
-  return (
-    <div>
-      <h1>{data.name}</h1>
-      <Button block uppercase onClick={selectPackage}>
-        {"Select Package"}
-      </Button>
-    </div>
-  );
-};
-
 const Custom = () => {
   const { t } = useTranslation();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
   const [onlyActivated, setOnlyActivated] = useState(false);
   const [packages, setPackages] = useState([]);
@@ -65,32 +29,96 @@ const Custom = () => {
     });
   }, [onlyActivated]);
 
+  const selectPackage = useCallback(
+    (languagePackage) => {
+      dispatch(clearQuery());
+      dispatch(
+        setLearnedPackage({
+          foreignWordLanguage: languagePackage.foreignWordLanguage,
+          translatedWordLanguage: languagePackage.translatedWordLanguage,
+          //using fixed value until server gives us this property
+          languagePackageId: languagePackage.id,
+          groupIds: [],
+          vocabsToday: 0,
+          staged: false,
+          onlyActivated: onlyActivated,
+          customLearning: true,
+        })
+      );
+
+      history.push("/learn/selection/custom/");
+    },
+    [dispatch, history, onlyActivated]
+  );
+
   const onChangeOnlyActivated = useCallback(() => {
     setOnlyActivated(!onlyActivated);
 
     // refetch packages
     getPackages(false, false, onlyActivated).then((response) => {
+      console.log(response.data);
       setPackages(response.data);
     });
   }, [onlyActivated]);
 
+  const columns = useMemo(
+    () => [
+      {
+        Header: t("screens.allGroups.groupName"),
+        accessor: "name",
+        Cell: ({ row }) => row.original.name,
+        headerClassName: "w-25",
+      },
+      {
+        Header: t("screens.allGroups.groupDescription"),
+        accessor: "description",
+        Cell: ({ row }) => (
+          <>
+            <p
+              className="group-description-cell"
+              data-tip={row.original.description}
+              data-for={`description-tooltip-${row.original.id}`}
+            >
+              {row.original.description}
+            </p>
+            <Tooltip id={`description-tooltip-${row.original.id}`} />
+          </>
+        ),
+        headerClassName: "w-50",
+      },
+      {
+        Header: "",
+        accessor: "select",
+        Cell: ({ row }) => (
+          <Button
+            block
+            uppercase
+            onClick={() => {
+              selectPackage(row.original);
+            }}
+          >
+            Start
+          </Button>
+        ),
+      },
+    ],
+    [selectPackage, t]
+  );
+
   return (
-    <div className="custom">
-      <div>
-        <h1>{t("screens.custom.title")}</h1>
-        <Switch
-          appearance="on-off"
-          optionLeft={"Only activated vocabs"}
-          onChange={onChangeOnlyActivated}
-          checked={onlyActivated}
-        />
-        {packages.map((languagePackage, index) => (
-          <CustomLearningPackageOverview
-            key={index}
-            data={languagePackage}
-            onlyActivated={onlyActivated}
+    <div className="custom-learning">
+      <div className="custom-learning-wrapper">
+        <div className="custom-learning-switch-wrapper">
+          <Switch
+            appearance="on-off"
+            optionLeft={"Only activated vocabs"}
+            onChange={onChangeOnlyActivated}
+            checked={onlyActivated}
           />
-        ))}
+        </div>
+        <div className="table-wrapper">
+          <Table columns={columns} data={packages} />
+        </div>
       </div>
     </div>
   );
